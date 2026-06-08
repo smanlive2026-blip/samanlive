9d70b3bffb06f2c9e619d49a9cbcebecfd2c383e
superlivein2026-max <tera-github-email@gmail.com>
2026-06-08 20:04:52 +0530

    Fix Module model import case

--- server/routes/area-manager.js
+++ server/routes/area-manager.js
@@ -4,7 +4,7 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const Manager = require('../models/Manager');
 const Shop = require('../models/Shop');
-const Module = require('../models/Module');
+const Module = require('../models/module');
 
 const JWT_SECRET = process.env.JWT_SECRET || 'samanlive-area-manager-secret-2026';
 
