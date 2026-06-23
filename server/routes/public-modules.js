84f04f758cc7450aaf9a701125f6638e1c2e70b8
superlivein2026-max <tera-github-email@gmail.com>
2026-06-23 12:26:43 +0530

    Fixed: Removed seed-modules.json from public-modules

--- server/routes/public-modules.js
+++ server/routes/public-modules.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const modulesData = require('../seed/seed-modules.json');
+// const modulesData = require('../seed/seed-modules.json'); <-- HATA DIYA
 
+// Abhi ke liye empty array bhej rahe hai. Baad me DB se lena
 router.get('/modules', (req, res) => {
-    res.json(modulesData.modules);
+    res.json([]); 
 });
 
 router.post('/modules/nearby', (req, res) => {
-    res.json(modulesData.modules);
+    res.json([]);
 });
 
 module.exports = router;
