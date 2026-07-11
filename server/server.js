ea4f36faa12400a21930a41c258f1a713b2b2ff7
superlivein2026-max <tera-github-email@gmail.com>
2026-07-11 18:53:24 +0530

    Updated shop page: removed tabs, clickable list view

--- server/server.js
+++ server/server.js
@@ -109,6 +109,7 @@ app.use('/api/local-market', require('./routes/shopRoutes'));
 // ==================== LOCATION ROUTES new ====================
 const locationRoutes = require('./modules/locationRoutes');
 app.use('/api/location', locationRoutes);
+app.use('/api/location', require('./modules/locationRoutes'));
 
 // ❌ PURANA CLAIM SYSTEM WALA FILE HATA DIYA
 // app.use('/api', require('./routes/shop'));
