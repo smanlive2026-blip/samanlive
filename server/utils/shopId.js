4a497c1ab1806ad2d57ea23bce751cbd0b36ca7f
superlivein2026-max <tera-github-email@gmail.com>
2026-07-23 19:47:11 +0530

    Updated shop page: removed tabs, clickable list view

--- server/utils/shopId.js
+++ server/utils/shopId.js
@@ -1,4 +1,4 @@
-// shopId ko hamesha String me convert kar dega
+// shopId ko hamesha String me convert kar dega     server/utils/shopId.js 
 const getShopId = (req) => String(req.params.shopId);
 
 module.exports = { getShopId };
